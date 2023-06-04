f9ff5a9aa4a7ebf5f842976b56453ab2beb3231f
BeefCutlet <whysabi492@naver.com>
2023-06-04 17:52:42 +0900

    [DOCS] Start Project

---
+++ src/test/java/tdd/practice/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.practice.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
