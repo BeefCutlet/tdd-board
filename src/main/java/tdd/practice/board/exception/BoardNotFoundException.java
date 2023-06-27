242161b9a66932ce94dbc73b20e16eec176a6bd1
BeefCutlet <whysabi492@naver.com>
2023-06-27 23:59:18 +0900

    [DOCS] 회원, 게시글 조회 실패 예외 추가

---
+++ src/main/java/tdd/practice/board/exception/BoardNotFoundException.java
@@ -0,0 +1,15 @@
+package tdd.practice.board.exception;
+
+public class BoardNotFoundException extends RuntimeException {
+
+    public BoardNotFoundException() {
+    }
+
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+
+    public BoardNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
