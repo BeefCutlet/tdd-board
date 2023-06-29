fe85d608112358d00fc613ded777f6f1bc772d05
BeefCutlet <whysabi492@naver.com>
2023-06-29 23:01:27 +0900

    [TEST] 페이징 및 키워드 검색 테스트 추가

--- src/test/java/tdd/practice/board/repository/BoardRepositoryImplTest.java
+++ src/test/java/tdd/practice/board/repository/BoardRepositoryImplTest.java
@@ -7,8 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import tdd.practice.board.dto.Board;
 import tdd.practice.board.dto.Member;
+import tdd.practice.board.util.pager.PageInfo;
+import tdd.practice.board.util.pager.Pager;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -80,5 +84,98 @@ class BoardRepositoryImplTest {
             assertThat(boardList.get(i).getBoardContent()).isEqualTo("content-" + (i+1));
             assertThat(boardList.get(i).getBoardCategory()).isEqualTo("category-" + (i+1));
         }
+    }
+
+    @Test
+    void searchWithNoKeyword() {
+        Member member = new Member();
+        memberRepository.save(member);
+
+        for (int i = 0; i < 31; i++) {
+            Board board = new Board();
+            board.setBoardWriter(member.getMemberNo());
+            board.setBoardTitle("title-" + (i+1));
+            board.setBoardContent("content-" + (i+1));
+            board.setBoardCategory("category-" + (i+1));
+            board.setBoardSecret(1);
+            board.setBoardGroup(0);
+            board.setBoardOrder(0);
+            board.setBoardLevel(0);
+            boardRepository.save(board);
+        }
+
+        Map<String, String> map = new HashMap<>();
+        Integer totalBoard = boardRepository.findCount(map);
+        assertThat(totalBoard).isEqualTo(31);
+
+        Pager pager = Pager.builder()
+                .currentPage(2)
+                .pageSize(10)
+                .blockSize(5)
+                .totalBoard(totalBoard)
+                .build();
+        PageInfo pageInfo = pager.getPageInfo();
+
+        assertThat(pageInfo.getStartPage()).isEqualTo(1);
+        assertThat(pageInfo.getEndPage()).isEqualTo(4);
+        assertThat(pageInfo.getStartRow()).isEqualTo(11);
+        assertThat(pageInfo.getEndRow()).isEqualTo(20);
+        assertThat(pageInfo.getPageSize()).isEqualTo(10);
+
+        List<Board> boardList = boardRepository.findAllBySearchCondition(pageInfo);
+        for (Board board : boardList) {
+            log.info("boardTitle = {}", board.getBoardTitle());
+        }
+        assertThat(boardList.get(0).getBoardTitle()).isEqualTo("title-21");
+    }
+
+    @Test
+    void searchWithKeyword() {
+        Member member = new Member();
+        memberRepository.save(member);
+
+        for (int i = 0; i < 31; i++) {
+            Board board = new Board();
+            board.setBoardWriter(member.getMemberNo());
+            board.setBoardTitle("title-" + (i+1));
+            board.setBoardContent("content-" + (i+1));
+            board.setBoardCategory("category-" + (i+1));
+            board.setBoardSecret(1);
+            board.setBoardGroup(0);
+            board.setBoardOrder(0);
+            board.setBoardLevel(0);
+            boardRepository.save(board);
+            log.info("boardTitle={}", board.getBoardTitle());
+        }
+
+        Map<String, String> searchCondition = new HashMap<>();
+        searchCondition.put("condition", "board_title");
+        searchCondition.put("keyword", "tle-1");
+        Integer totalBoard = boardRepository.findCount(searchCondition);
+        assertThat(totalBoard).isEqualTo(11);
+
+        Pager pager = Pager.builder()
+                .currentPage(1)
+                .pageSize(10)
+                .blockSize(5)
+                .totalBoard(totalBoard)
+                .condition(searchCondition.get("condition"))
+                .keyword(searchCondition.get("keyword"))
+                .build();
+        PageInfo pageInfo = pager.getPageInfo();
+        log.info("condition={}", pageInfo.getCondition());
+        log.info("keyword={}", pageInfo.getKeyword());
+
+        assertThat(pageInfo.getStartPage()).isEqualTo(1);
+        assertThat(pageInfo.getEndPage()).isEqualTo(2);
+        assertThat(pageInfo.getStartRow()).isEqualTo(1);
+        assertThat(pageInfo.getEndRow()).isEqualTo(10);
+        assertThat(pageInfo.getPageSize()).isEqualTo(10);
+
+        List<Board> boardList = boardRepository.findAllBySearchCondition(pageInfo);
+        for (Board board : boardList) {
+            log.info("boardTitle = {}", board.getBoardTitle());
+        }
+        assertThat(boardList.get(0).getBoardTitle()).isEqualTo("title-19");
     }
 }
