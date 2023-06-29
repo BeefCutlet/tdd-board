2170327ab96ad050e214dc73e653108346810d80
BeefCutlet <whysabi492@naver.com>
2023-06-29 23:01:04 +0900

    [TEST] 이메일 전송 테스트 수신자 이메일을 properties 파일에서 받도록 수정

--- src/test/java/tdd/practice/board/email/EmailSendTest.java
+++ src/test/java/tdd/practice/board/email/EmailSendTest.java
@@ -1,19 +1,27 @@
 package tdd.practice.board.email;
 
 import org.junit.jupiter.api.Test;
-import tdd.practice.board.util.EmailMessage;
-import tdd.practice.board.util.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import tdd.practice.board.util.emailsender.EmailMessage;
+import tdd.practice.board.util.emailsender.EmailSender;
 
 import javax.mail.MessagingException;
 
+@SpringBootTest
 public class EmailSendTest {
 
-    private EmailSender emailSender = new EmailSender();
+    @Autowired
+    private EmailSender emailSender;
+
+    @Value("${email.recipient}")
+    private String emailRecipient;
 
     @Test
     void emailSend() throws MessagingException {
         EmailMessage emailMessage = EmailMessage.builder()
-                                        .recipient("seuk7316@gmail.com")
+                                        .recipient(emailRecipient)
                                         .subject("테스트 제목")
                                         .content("테스트 내용")
                                         .build();
