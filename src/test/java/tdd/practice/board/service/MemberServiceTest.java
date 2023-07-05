6f6f4c25d4a79040973493ab45f1f10c0053d111
BeefCutlet <whysabi492@naver.com>
2023-07-06 01:34:52 +0900

    [TEST] MemberServiceTest 추가

---
+++ src/test/java/tdd/practice/board/service/MemberServiceTest.java
@@ -0,0 +1,70 @@
+package tdd.practice.board.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tdd.practice.board.dto.Member;
+import tdd.practice.board.exception.MemberNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    void saveAndFindMemberSuccess() {
+        Member member = new Member();
+        member.setMemberEmail("TEST EMAIL");
+        member.setMemberNickname("TEST NICKNAME");
+        member.setMemberPassword("TEST PASSWORD");
+
+        Member savedMember = memberService.save(member);
+        log.info("memberNo={}", savedMember.getMemberNo());
+        Member findMember = memberService.findMember(savedMember.getMemberNo());
+
+        assertThat(findMember.getMemberEmail()).isEqualTo("TEST EMAIL");
+        assertThat(findMember.getMemberNickname()).isEqualTo("TEST NICKNAME");
+        assertThat(findMember.getMemberPassword()).isEqualTo("TEST PASSWORD");
+    }
+
+    @Test
+    void saveAndFindMemberFailed() {
+        Member member = new Member();
+        member.setMemberEmail("TEST EMAIL");
+        member.setMemberNickname("TEST NICKNAME");
+        member.setMemberPassword("TEST PASSWORD");
+
+        Member savedMember = memberService.save(member);
+        log.info("memberNo={}", savedMember.getMemberNo());
+
+        assertThatThrownBy(() -> memberService.findMember(savedMember.getMemberNo()+1))
+                .isInstanceOf(MemberNotFoundException.class);
+    }
+
+    @Test
+    void updateMember() {
+        Member member = new Member();
+        member.setMemberEmail("TEST EMAIL");
+        member.setMemberNickname("TEST NICKNAME");
+        member.setMemberPassword("TEST PASSWORD");
+        Member savedMember = memberService.save(member);
+        log.info("memberNo={}", savedMember.getMemberNo());
+
+        savedMember.setMemberNickname("TEST NICKNAME UPDATED");
+        savedMember.setMemberPassword("TEST PASSWORD UPDATED");
+        log.info("memberNickname={}", savedMember.getMemberNickname());
+        log.info("memberPassword={}", savedMember.getMemberPassword());
+        memberService.update(savedMember);
+
+        Member findMember = memberService.findMember(savedMember.getMemberNo());
+        assertThat(findMember.getMemberEmail()).isEqualTo("TEST EMAIL");
+        assertThat(findMember.getMemberNickname()).isEqualTo("TEST NICKNAME UPDATED");
+        assertThat(findMember.getMemberPassword()).isEqualTo("TEST PASSWORD UPDATED");
+    }
+}
