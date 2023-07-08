30eaa31b108d7dce987b3df86961ea198595028b
BeefCutlet <whysabi492@naver.com>
2023-07-08 23:36:51 +0900

    [FEAT] 로그인, 비밀번호 찾기, 비밀번호 수정 요청처리 메서드 추가

--- src/main/java/tdd/practice/board/controller/LoginController.java
+++ src/main/java/tdd/practice/board/controller/LoginController.java
@@ -1,8 +1,31 @@
 package tdd.practice.board.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tdd.practice.board.dto.Member;
+import tdd.practice.board.service.MemberService;
 
 @RestController
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody Member member) {
+
+        return "";
+    }
+
+    @GetMapping("/password/find")
+    public Member findPassword(@RequestBody Member member) {
+        Member findMember = memberService.findMember(member.getMemberNo());
+        return findMember;
+    }
+
+    @PutMapping("/password/modify")
+    public String modifyMemberInfo(@RequestBody Member member) {
+        memberService.update(member);
+        return "OK";
+    }
 }
