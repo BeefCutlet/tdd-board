f283eb53059ab775c4f9cee94e626c28cab051e0
BeefCutlet <whysabi492@naver.com>
2023-07-23 02:08:57 +0900

    [ETC] 필요없는 import 라이브러리 제거

--- src/main/java/tdd/practice/board/config/SecurityConfig.java
+++ src/main/java/tdd/practice/board/config/SecurityConfig.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,8 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import tdd.practice.board.security.filter.JwtLoginProcessingFilter;
 import tdd.practice.board.security.handler.JwtLoginFailureHandler;
 import tdd.practice.board.security.handler.JwtLoginSuccessHandler;
-import tdd.practice.board.security.provider.JwtAuthenticationProvider;
-import tdd.practice.board.security.service.CustomUserDetailsService;
 
 @Configuration
 @RequiredArgsConstructor
