0d6ca3f7a8a3745cc730939c19a131aa33e77866
BeefCutlet <whysabi492@naver.com>
2023-07-25 21:20:14 +0900

    [DEBUG] JWT 유효성 검증 필터 null 검증 우선을 위한 코드 위치 변경

--- src/main/java/tdd/practice/board/security/filter/JwtVerifyFilter.java
+++ src/main/java/tdd/practice/board/security/filter/JwtVerifyFilter.java
@@ -3,6 +3,7 @@ package tdd.practice.board.security.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.web.servlet.OAuth2ClientDsl;
 import org.springframework.web.filter.OncePerRequestFilter;
 import tdd.practice.board.util.jwt.JwtUtil;
 
@@ -20,22 +21,23 @@ public class JwtVerifyFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("JwtVerifyFilter 시작");
         String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
         log.info("authorization={}", authorization);
-        String token = authorization.split(" ")[1];
 
         //유효성 검사
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-            log.error("Authorization을 잘못 보냈습니다.");
+            log.error("Authorization가 유효하지 않습니다.");
             filterChain.doFilter(request, response);
             return;
         }
+        String token = authorization.split(" ")[1];
 
         //만료기간 검사 + SecretKey 유효성 검사(실패 시 SignatureException 발생)
         boolean isExpired = JwtUtil.isExpired(token, secretKey);
         if (isExpired) {
+            log.error("Token is Expired");
             filterChain.doFilter(request, response);
-            return;
         }
     }
 }
